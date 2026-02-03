3204172b3beeb8c72c098953a841848cb5dbd09c
jeelhappypet <jeel@happypet.care>
2026-02-03 10:53:11 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Courseform from './components/Courseform';
 
 function App() {
   return (
     <div className='font-GoogleSans'>
-      <Courseform/>
+      
     </div>
   );
 }
